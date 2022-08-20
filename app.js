61fde42c18a83b6a9c6c286bec2bb8aee29cb454
Madhav Goyal <57744109+MadhavGoyal-6@users.noreply.github.com>
2022-08-20 13:10:09 +0530

    Make procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const cors = require("cors")
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 const offerRouter = require("./routes/Offers")
 const answerRouter = require("./routes/Answers")
