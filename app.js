4f4cac94d416f7d711af7448e7051a5a80a155ff
Madhav Goyal <57744109+MadhavGoyal-6@users.noreply.github.com>
2022-08-21 15:35:15 +0530

    Add dotenv for custom env setup 👻

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const cors = require("cors")
 const app = express()
+require('dotenv').config()
 const PORT = process.env.PORT
 const fs = require("fs");
 
